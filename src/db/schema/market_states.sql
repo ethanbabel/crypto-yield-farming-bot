500965ba748c2c6e8cb55e634c938f30cb7a1e41
ethanbabel <babelethan@gmail.com>
2025-06-20 01:17:19 +0000

    allow market_state fields to be null in db

--- src/db/schema/market_states.sql
+++ src/db/schema/market_states.sql
@@ -3,35 +3,35 @@ CREATE TABLE IF NOT EXISTS market_states (
     market_id INTEGER NOT NULL REFERENCES markets(id),
     timestamp TIMESTAMPTZ NOT NULL DEFAULT now(),
 
-    borrowing_factor_long NUMERIC NOT NULL,
-    borrowing_factor_short NUMERIC NOT NULL,
+    borrowing_factor_long NUMERIC,
+    borrowing_factor_short NUMERIC,
 
-    pnl_long NUMERIC NOT NULL,
-    pnl_short NUMERIC NOT NULL,
-    pnl_net NUMERIC NOT NULL,
+    pnl_long NUMERIC,
+    pnl_short NUMERIC,
+    pnl_net NUMERIC,
 
-    gm_price_min NUMERIC NOT NULL,
-    gm_price_max NUMERIC NOT NULL,
-    gm_price_mid NUMERIC NOT NULL,
+    gm_price_min NUMERIC,
+    gm_price_max NUMERIC,
+    gm_price_mid NUMERIC,
 
-    pool_long_amount NUMERIC NOT NULL,
-    pool_short_amount NUMERIC NOT NULL,
-    pool_long_token_usd NUMERIC NOT NULL,
-    pool_short_token_usd NUMERIC NOT NULL,
+    pool_long_amount NUMERIC,
+    pool_short_amount NUMERIC,
+    pool_long_token_usd NUMERIC,
+    pool_short_token_usd NUMERIC,
 
-    open_interest_long NUMERIC NOT NULL,
-    open_interest_short NUMERIC NOT NULL,
-    open_interest_long_via_tokens NUMERIC NOT NULL,
-    open_interest_short_via_tokens NUMERIC NOT NULL,
+    open_interest_long NUMERIC,
+    open_interest_short NUMERIC,
+    open_interest_long_via_tokens NUMERIC,
+    open_interest_short_via_tokens NUMERIC,
 
-    utilization NUMERIC NOT NULL,
+    utilization NUMERIC,
 
-    swap_volume NUMERIC NOT NULL,
-    trading_volume NUMERIC NOT NULL,
+    swap_volume NUMERIC,
+    trading_volume NUMERIC,
 
-    fees_position NUMERIC NOT NULL,
-    fees_liquidation NUMERIC NOT NULL,
-    fees_swap NUMERIC NOT NULL,
-    fees_borrowing NUMERIC NOT NULL,
-    fees_total NUMERIC NOT NULL
+    fees_position NUMERIC,
+    fees_liquidation NUMERIC,
+    fees_swap NUMERIC,
+    fees_borrowing NUMERIC,
+    fees_total NUMERIC
 );

e3c7ee8846191afd11394b489636f50ac38d310e
ethanbabel <babelethan@gmail.com>
2026-02-02 16:16:02 +0000

    modify db connection s.t. slow statement execution warning logs are only logged after long threshold as opposed to default 1s

--- src/db/connection.rs
+++ src/db/connection.rs
@@ -1,14 +1,22 @@
+use sqlx::ConnectOptions;
 use sqlx::postgres::{
+    PgConnectOptions,
     PgPool,
     PgPoolOptions,
 };
 use eyre::Result;
+use std::str::FromStr;
+use std::time::Duration;
+use tracing::log::LevelFilter;
 
 use crate::config::Config;
 
 pub async fn create_pool(config: &Config) -> Result<PgPool, sqlx::Error> {
+    let connect_options = PgConnectOptions::from_str(&config.database_url)?
+        .log_slow_statements(LevelFilter::Warn, Duration::from_secs(60));
+
     PgPoolOptions::new()
         .max_connections(5) 
-        .connect(&config.database_url)
+        .connect_with(connect_options)
         .await
 }

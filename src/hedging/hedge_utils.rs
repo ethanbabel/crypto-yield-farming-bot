78f508b27c62d6c8167aa904077d3f239c679b29
ethanbabel <babelethan@gmail.com>
2026-05-06 19:31:23 +0000

    additional dydx perp symbol <-> token symbol helpers to clarify logs

--- src/hedging/hedge_utils.rs
+++ src/hedging/hedge_utils.rs
@@ -13,10 +13,18 @@ const TOKEN_MAP: [(&str, &str); 4] = [
     ("tBTC", "BTC"),
 ];
 
-pub fn get_dydx_perp_ticker(token_symbol: &str) -> String {
-    let base = TOKEN_MAP.iter()
+pub fn get_dydx_perp_base_symbol(token_symbol: &str) -> String {
+    TOKEN_MAP.iter()
         .find(|(key, _)| *key == token_symbol)
         .map(|(_, value)| *value)
-        .unwrap_or(token_symbol);
-    format!("{}-USD", base)
+        .unwrap_or(token_symbol)
+        .to_string()
+}
+
+pub fn get_dydx_perp_ticker(token_symbol: &str) -> String {
+    format!("{}-USD", get_dydx_perp_base_symbol(token_symbol))
+}
+
+pub fn get_token_symbol_for_dydx_perp_ticker(ticker: &str) -> Option<String> {
+    ticker.strip_suffix("-USD").map(|symbol| symbol.to_string())
 }
